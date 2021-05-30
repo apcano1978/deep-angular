89d02050bebbaa2ce0d6aacee1d578816553248a
Antonio Pérez <apcano1978@gmail.com>
2021-05-30 19:06:01 +0200

    observable description

--- src/app/public/home/containers/home.component.ts
+++ src/app/public/home/containers/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { PublicService } from '../../public.service';
 
 @Component({
@@ -7,27 +8,51 @@ import { PublicService } from '../../public.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  characters;
-  continents;
+  characters = [];
+  continents = [];
 
   constructor(private publicService: PublicService) {}
 
   ngOnInit() {
-    this.getCharacters();
-    this.getContinents();
+    // this.getCharacters();
+    // this.getContinents();
+    this.myVeryFirstObservable();
   }
 
-  getCharacters() {
-    this.publicService.getCharacters().subscribe(
-      response => this.characters = response,
-      error => console.log(error)
-    )
+  myVeryFirstObservable() {
+    const observable = new Observable(subscriber => {
+      let count = 0;
+
+      setInterval(() => {
+        subscriber.next(count);
+        count += 1;
+      }, 1000);
+    });
+
+    console.log('Antes de ejecutar el observable...');
+    const subscription = observable.subscribe(
+      value => console.log('Callback next', value),
+      error => console.log('Callback error', error),
+      () => console.log('Observable completado')
+    );
+    console.log('Después de ejecutar el observable...');
+
+    setTimeout(() => {
+      subscription.unsubscribe();
+    }, 3500)
   }
 
-  getContinents() {
-    this.publicService.getContinents().subscribe(
-      response => this.continents = response,
-      error => console.log(error)
-    )
-  }
+  // getCharacters() {
+  //   this.publicService.getCharacters().subscribe(
+  //     response => this.characters = response,
+  //     error => console.log(error)
+  //   )
+  // }
+
+  // getContinents() {
+  //   this.publicService.getContinents().subscribe(
+  //     response => this.continents = response,
+  //     error => console.log(error)
+  //   )
+  // }
 }
